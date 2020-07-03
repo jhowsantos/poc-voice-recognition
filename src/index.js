c176001fbb5c259b2b6da71cc005781b926ef29d
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-07-03 16:28:16 -0300

    add integração com API de CEP

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import {
-  StyleSheet,
   View,
   TouchableOpacity,
   Text,
-  ScrollView,
+  ActivityIndicator,
   Image,
 } from 'react-native';
 import Voice from '@react-native-community/voice';
 
+import styles from './styles';
+
 const Home = () => {
-  const [started, setStarted] = useState('');
-  const [results, setResults] = useState([]);
-  const [partialResults, setPartialResults] = useState([]);
-  const [pitch, setPitch] = useState('');
-  const [error, setError] = useState('');
-  const [end, setEnd] = useState('');
+  const [cepVoice, setCepVoice] = useState('');
+  const [dataAddress, setDataAddress] = useState({});
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     function onSpeechStart(e) {
       console.log('onSpeechStart: ', e);
-      setStarted('recording');
+      setAnimate(true);
     }
+
     function onSpeechResults(e) {
       console.log('onSpeechResults: ', e);
-      setResults(e.value);
+      setCepVoice(e.value[0]);
     }
 
     Voice.onSpeechStart = onSpeechStart;
@@ -36,13 +35,6 @@ const Home = () => {
   }, []);
 
   const startRecognizing = async () => {
-    setPitch('');
-    setError('');
-    setStarted('');
-    setResults([]);
-    setPartialResults([]);
-    setEnd('');
-
     try {
       await Voice.start('pt-BR');
     } catch (e) {
@@ -59,33 +51,110 @@ const Home = () => {
     }
   };
 
+  const getAddressData = async () => {
+    const cep = cepVoice.trim();
+
+    await fetch(`https://api.pagar.me/1/zipcodes/${cep}`, {method: 'get'})
+      .then((res) => {
+        console.log(res);
+        if (res.ok) {
+          return res.json();
+        }
+
+        if (!res.ok && cep.length > 0) {
+          alert('CEP inválido');
+          stopRecognizing();
+          setAnimate(false);
+        }
+
+        if (!res.ok && cep.length === 0) {
+          setDataAddress({});
+          stopRecognizing();
+          setAnimate(false);
+        }
+      })
+      .then((res) => {
+        if (res !== undefined) {
+          console.log(res);
+          setDataAddress(res);
+          stopRecognizing();
+          setAnimate(false);
+        }
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.containerInfosCEP}>
-        <Text style={styles.title}>
-          {started === ''
-            ? 'Toque no botão para falar e pesquisar pelo seu CEP'
-            : 'Informações do CEP informado'}
-        </Text>
+      {cepVoice === '' && (
+        <View style={styles.containerInfosCEP}>
+          <Text style={styles.title}>
+            Toque no botão para falar e pesquisar pelo seu CEP
+          </Text>
+        </View>
+      )}
 
-        <ScrollView>
-          {partialResults.map((result, index) => {
-            return (
-              <Text
-                key={`partial-result-${index}`}
-                style={styles.partialResult}>
-                {result}
+      {cepVoice === '' ? (
+        <View style={styles.containerInfosCEP}>
+          <ActivityIndicator animating={animate} size="large" color="#aa00aa" />
+        </View>
+      ) : (
+        <View style={styles.containerInfosCEP}>
+          <Text style={styles.title}>Informações do CEP informado</Text>
+
+          <View style={styles.containerDataAddress}>
+            <View style={styles.containerLabel}>
+              <Text style={styles.label}>CEP informado:</Text>
+            </View>
+            <View style={styles.containerTextDataAddress}>
+              <Text style={styles.textDataAddress}>{dataAddress.zipcode}</Text>
+            </View>
+          </View>
+
+          <View style={styles.containerDataAddress}>
+            <View style={styles.containerLabel}>
+              <Text style={styles.label}>Estado:</Text>
+            </View>
+            <View style={styles.containerTextDataAddress}>
+              <Text style={styles.textDataAddress}>{dataAddress.state}</Text>
+            </View>
+          </View>
+
+          <View style={styles.containerDataAddress}>
+            <View style={styles.containerLabel}>
+              <Text style={styles.label}>Cidade:</Text>
+            </View>
+            <View style={styles.containerTextDataAddress}>
+              <Text style={styles.textDataAddress}>{dataAddress.city}</Text>
+            </View>
+          </View>
+
+          <View style={styles.containerDataAddress}>
+            <View style={styles.containerLabel}>
+              <Text style={styles.label}>Bairro:</Text>
+            </View>
+            <View style={styles.containerTextDataAddress}>
+              <Text style={styles.textDataAddress}>
+                {dataAddress.neighborhood}
               </Text>
-            );
-          })}
-        </ScrollView>
-      </View>
+            </View>
+          </View>
+
+          <View style={styles.containerDataAddress}>
+            <View style={styles.containerLabel}>
+              <Text style={styles.label}>Rua:</Text>
+            </View>
+            <View style={styles.containerTextDataAddress}>
+              <Text style={styles.textDataAddress}>{dataAddress.street}</Text>
+            </View>
+          </View>
+        </View>
+      )}
 
       <View style={styles.containerButton}>
         <TouchableOpacity
           style={styles.button}
           onPressIn={startRecognizing}
-          onPressOut={stopRecognizing}>
+          onPressOut={getAddressData}>
           <Image
             source={{
               uri:
@@ -98,46 +167,5 @@ const Home = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-    paddingTop: 35,
-  },
-  containerInfosCEP: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    backgroundColor: '#FFF',
-  },
-  containerButton: {
-    flex: 1.5,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    width: 170,
-    height: 170,
-    borderRadius: 90,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: '#aa00aa',
-    borderWidth: 2,
-  },
-  image: {
-    width: 100,
-    height: 100,
-  },
-  title: {
-    fontSize: 22,
-    color: '#aa00aa',
-    fontWeight: '700',
-    textAlign: 'center',
-  },
-});
 
 export default Home;
